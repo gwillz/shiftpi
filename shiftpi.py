555e0bdcc0495400e206607ee8df0fe48acc00c6
gwillz <gwillz@internode.on.net>
2016-03-31 14:55:26 +0000

    modified into a class type - no globals

--- shiftpi.py
+++ shiftpi.py
@@ -1,137 +1,69 @@
-'''
-A library that allows simple access to 74HC595 shift registers on a Raspberry Pi using any digital I/O pins.
-'''
+#!/usr/bin/env python2
 
+import RPi.GPIO as GPIO, time
 
-import RPi.GPIO as GPIO
-from time import sleep
+class ShiftPi(object):
+    
+    # Define MODES
+    ALL  = -1
+    HIGH = 1
+    LOW  = 0
+    
+    # is used to store states of all pins
+    _registers = []
 
-GPIO.setmode(GPIO.BCM)
-
-version = "0.2"
-version_info = (0, 2)
-
-# Define MODES
-ALL  = -1
-HIGH = 1
-LOW  = 0
-
-# Define pins
-_SER_pin   = 25    #pin 14 on the 75HC595
-_RCLK_pin  = 24    #pin 12 on the 75HC595
-_SRCLK_pin = 23   #pin 11 on the 75HC595
-
-# is used to store states of all pins
-_registers = list()
-
-#How many of the shift registers - you can change them with shiftRegisters method
-_number_of_shiftregisters = 1
-
-def pinsSetup(**kwargs):
-    '''
-    Allows the user to define custom pins
-    '''
-    global _SER_pin, _RCLK_pin, _SRCLK_pin
-
-    custompins = 0
-    serpin = _SER_pin
-    rclkpin = _RCLK_pin
-    srclkpin = _SRCLK_pin
-
-    if len(kwargs) > 0:
-        custompins = 1
-
-        _SER_pin = kwargs.get('ser', _SER_pin)
-        _RCLK_pin = kwargs.get('rclk', _RCLK_pin)
-        _SRCLK_pin = kwargs.get('srclk', _SRCLK_pin)
-
-    if custompins:
-        if _SER_pin != serpin or _RCLK_pin != rclkpin or _SRCLK_pin != srclkpin:
-            GPIO.setwarnings(True)
-    else:
+    def __init__(self, ser_pin=27, sck_pin=22, rck_pin=23, num_registers=1):
+        self._SER_pin   = ser_pin
+        self._RCLK_pin  = rck_pin
+        self._SRCLK_pin = sck_pin
+        self._REG_num   = num_registers
+        
+        GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-
-    GPIO.setup(_SER_pin, GPIO.OUT)
-    GPIO.setup(_RCLK_pin, GPIO.OUT)
-    GPIO.setup(_SRCLK_pin, GPIO.OUT)
-
-def startupMode(mode, execute = False):
-    '''
-    Allows the user to change the default state of the shift registers outputs
-    '''
-    if isinstance(mode, int):
-        if mode is HIGH or mode is LOW:
-            _all(mode, execute)
+        GPIO.setup(self._SER_pin,   GPIO.OUT)
+        GPIO.setup(self._RCLK_pin,  GPIO.OUT)
+        GPIO.setup(self._SRCLK_pin, GPIO.OUT)
+    
+    def write(self, pin, mode):
+        if pin == self.ALL:
+            self._all(mode)
         else:
-            raise ValueError("The mode can be only HIGH or LOW or Dictionary with specific pins and modes")
-    elif isinstance(mode, dict):
-        for pin, mode in mode.iteritems():
-            _setPin(pin, mode)
+            if len(self._registers) == 0:
+                self._all(self.LOW)
+
+            self._setPin(pin, mode)
+        self._execute()
+
+    def _all_pins(self):
+        return self._REG_num * 8
+
+    def _all(self, mode, execute=True):
+        all_shr = self._all_pins()
+
+        for pin in range(0, all_shr):
+            self._setPin(pin, mode)
         if execute:
-            _execute()
-    else:
-        raise ValueError("The mode can be only HIGH or LOW or Dictionary with specific pins and modes")
+            self._execute()
 
+        return self._registers
 
-def shiftRegisters(num):
-    '''
-    Allows the user to define the number of shift registers are connected
-    '''
-    global _number_of_shiftregisters
-    _number_of_shiftregisters = num
-    _all(LOW)
+    def _setPin(self, pin, mode):
+        try:
+            self._registers[pin] = mode
+        except IndexError:
+            self._registers.insert(pin, mode)
 
-def digitalWrite(pin, mode):
-    '''
-    Allows the user to set the state of a pin on the shift register
-    '''
-    if pin == ALL:
-        _all(mode)
-    else:
-        if len(_registers) == 0:
-            _all(LOW)
+    def _execute(self):
+        all_pins = self._all_pins()
+        GPIO.output(self._RCLK_pin, GPIO.LOW)
 
-        _setPin(pin, mode)
-    _execute()
+        for pin in range(all_pins -1, -1, -1):
+            GPIO.output(self._SRCLK_pin, GPIO.LOW)
 
-def delay(millis):
-    '''
-    Used for creating a delay between commands
-    '''
-    millis_to_seconds = float(millis)/1000
-    return sleep(millis_to_seconds)
+            pin_mode = self._registers[pin]
 
-def _all_pins():
-    return _number_of_shiftregisters * 8
+            GPIO.output(self._SER_pin, pin_mode)
+            GPIO.output(self._SRCLK_pin, GPIO.HIGH)
 
-def _all(mode, execute = True):
-    all_shr = _all_pins()
+        GPIO.output(self._RCLK_pin, GPIO.HIGH)
 
-    for pin in range(0, all_shr):
-        _setPin(pin, mode)
-    if execute:
-        _execute()
-
-    return _registers
-
-def _setPin(pin, mode):
-    try:
-        _registers[pin] = mode
-    except IndexError:
-        _registers.insert(pin, mode)
-
-def _execute():
-    all_pins = _all_pins()
-    GPIO.output(_RCLK_pin, GPIO.LOW)
-
-    for pin in range(all_pins -1, -1, -1):
-        GPIO.output(_SRCLK_pin, GPIO.LOW)
-
-        pin_mode = _registers[pin]
-
-        GPIO.output(_SER_pin, pin_mode)
-        GPIO.output(_SRCLK_pin, GPIO.HIGH)
-
-    GPIO.output(_RCLK_pin, GPIO.HIGH)
-
-pinsSetup()
