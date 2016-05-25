6b1c7467ab36c4c4b1485af5bc1e1b3411f83702
gwillz <gwillz@internode.on.net>
2016-05-25 23:00:03 +0930

    updated setup.py descriptions

--- setup.py
+++ setup.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python
 
-from __future__ import with_statement
-import distutils.core, os
+from setuptools import setup
 
-try:
-    import setuptools
-except ImportError:
-    pass
+def get_long_description(fname):
+    try:
+        import pypandoc
+        return pypandoc.convert(fname, 'rst')
+    except:
+        import os
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-distutils.core.setup(name='shiftpi',
+setup(name='shiftpi',
       version="0.3",
       description="ShiftPi is the easiest way to work with 74HC595 shift registers on your Raspberry Pi.",
       author='Gwilyn Saunders',
       author_email='gwilyn.saunders@mk2es.com.au',
       url='https://git.gwillz.com.au/mk2/shiftpi',
       packages=['shiftpi'],
-      long_description=read('README.md'),
+      long_description=get_long_description('README.md'),
       classifiers=[
           'Operating System :: POSIX',
           'Operating System :: POSIX :: BSD',
           'Operating System :: POSIX :: Linux',
           'Operating System :: Unix',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
           'Topic :: System :: Shells',
           'Topic :: Utilities',
