86445ce04a7258c993ece4ac750899e637bdc8d0
gwillz <gwillz@internode.on.net>
2016-10-15 09:35:23 +1030

    added pypi to CI

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 import os
 
-requirements = [i for i in open('requirements.txt').read().split() if 'git' not in i]
+requirements = [i for i in open('requirements.txt').read().split() if not i.startswith('--') and len(i) > 0]
 
 def get_long_description(fname):
     try:
@@ -13,13 +13,13 @@ def get_long_description(fname):
         return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='shiftpi',
-      version="0.3.3",
+      version="0.3.4",
       description="ShiftPi is the easiest way to work with 74HC595 shift registers on your Raspberry Pi.",
       author='Gwilyn Saunders',
       author_email='gwilyn.saunders@mk2es.com.au',
-      url='https://git.gwillz.com.au/mk2/shiftpi',
+      url='https://git.mk2es.com.au/mk2/shiftpi',
       packages=['shiftpi'],
-#      install_requires=requirements,
+      install_requires=requirements,
       long_description=get_long_description('README.md'),
       classifiers=[
           'Operating System :: POSIX',
